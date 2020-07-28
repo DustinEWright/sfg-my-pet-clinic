7601ebc4f382d8e09f98c88e797102d5231c39a4
Dustin Wright <nzeroxpd@gmail.com>
2020-07-28 16:35:29 -0500

    Added initial files to project.  Added John's .gitignore.

---
+++ src/main/java/guru/springframework/sfgmypetclinic/SfgMyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgmypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgMyPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgMyPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sfgmypetclinic/SfgMyPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgmypetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgMyPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
