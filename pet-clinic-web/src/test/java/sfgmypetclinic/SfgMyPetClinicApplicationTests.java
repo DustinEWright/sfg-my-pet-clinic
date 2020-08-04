277a5df10c8e60faf7b7569708381c84843578f8
Dustin Wright <nzeroxpd@gmail.com>
2020-08-03 20:13:28 -0500

    Have not compared to this commit as video 40 has an addendom addressing changes needed for later versions of Spring Boot >2.0.  refactoring to multimodule maven project. closes #1.

---
+++ pet-clinic-web/src/test/java/sfgmypetclinic/SfgMyPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package sfgmypetclinic;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgMyPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
